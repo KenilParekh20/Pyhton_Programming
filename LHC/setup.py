c581316d0bcc8ade76061f782b55750b674964d6
KenilParekh20 <kenilparekh51@gmail.com>
2025-09-06 23:55:05 +0530

    Long hour coding

---
+++ LHC/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name="signal_ICT_Kenil_parekh_92400133150",  
+    version="1.0.0",
+    packages=find_packages(),                  
+    install_requires=[    
+        "numpy",
+        "matplotlib"
+    ],
+    author  ="Kenil parekh",
+    description="Python package for signal processing exercises",
+    python_requires=">=3.6"
+)
