e96c03bbc181c62bb15c2392fb380ef6683a7eef
KenilParekh20 <kenilparekh51@gmail.com>
2025-11-08 20:33:17 +0530

    Post lab

--- Lab 17/1st.py
+++ Lab 17/1st.py
@@ -8,5 +8,3 @@ X_z=sp.summation(x_n*z**(-n),(n,0,sp.oo))
 
 print("Z-transform of x[n] = (1/2)^n u[n] is:")
 sp.pprint(X_z.simplify())
-
-
--- Lab 17/2nd.py
+++ Lab 17/2nd.py
@@ -4,7 +4,7 @@ n,z,w=sp.symbols('n z w')
 
 x_n=sp.cos(w*n)
 
-X_z=sp.summation(x_n*z**(-n),(n,0,sp.oo))
+X_z=sp.summation(x_n*z**(-n),(n,0,sp.oo))   
 
 print("Z-transform of x[n] = cos(w*n)u[n] is:")
 sp.pprint(X_z.simplify())
